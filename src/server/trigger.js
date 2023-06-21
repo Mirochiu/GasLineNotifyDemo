1a6f07694c51ef59a38136e7e05fb6d601509334
Yung-Hsiang Chiu <mirror.tw@gmail.com>
2023-06-21 09:17:30 +0800

    增加重試次數與延遲時間,迴避無法存取的問題
    Exception: Address unavailable: https://www.twse.com.tw/holidaySchedule/holidaySchedule?response=open_data

--- src/server/trigger.js
+++ src/server/trigger.js
@@ -1,106 +1,109 @@
 import { getConfig, log } from './sheets';
 import { notify } from './line';
 
-// https://developers.google.com/apps-script/reference/base/weekday
-const WEEKDAYS = [
-  ScriptApp.WeekDay.MONDAY,
-  ScriptApp.WeekDay.TUESDAY,
-  ScriptApp.WeekDay.WEDNESDAY,
-  ScriptApp.WeekDay.THURSDAY,
-  ScriptApp.WeekDay.FRIDAY,
-  ScriptApp.WeekDay.SATURDAY,
-  ScriptApp.WeekDay.SUNDAY,
-];
-
 // https://developers.google.com/apps-script/guides/triggers/events#time-driven_events
 const TRA_CH_TO_TRIGGER_DAYOF_WEEKEY = {
-  '一': 1,
-  '二': 2,
-  '三': 3,
-  '四': 4,
-  '五': 5,
-  '六': 6,
-  '日': 7,
+  一: 1,
+  二: 2,
+  三: 3,
+  四: 4,
+  五: 5,
+  六: 6,
+  日: 7,
 };
 
 const event2date = (e) => {
-  //作一下檢驗紀錄,避免後續Apps Script平台有改變策略
-  if ('UTC' !== e.timezone) log('trigger event.timezone not utc:', e.timezone);
-  let date = new Date(Date.UTC(
-    e.year, e.month - 1, e['day-of-month'],
-    e.hour, e.minute, e.second
-  ));
+  // 作一下檢驗紀錄,避免後續Apps Script平台有改變策略
+  if (e.timezone !== 'UTC') log('trigger event.timezone not utc:', e.timezone);
+  const date = new Date(
+    Date.UTC(e.year, e.month - 1, e['day-of-month'], e.hour, e.minute, e.second)
+  );
   return {
     date,
     event: e,
     hour: date.getHours(),
-    wday: date.getDay() == 0 ? 7 : date.getDay(),
+    wday: date.getDay() === 0 ? 7 : date.getDay(),
     year: date.getFullYear(),
     month: date.getMonth() + 1,
     day: date.getDate(),
   };
-}
+};
 
 const fetchAndFilterStockByNumber = (list) => {
-  const res = UrlFetchApp
-    .fetch('https://www.twse.com.tw/exchangeReport/STOCK_DAY_ALL?response=open_data');
-  return Utilities
-    .parseCsv(res.getContentText())
-    .filter(r => list.includes(r[0]))
-    .map(r => ({
+  const URL =
+    'https://www.twse.com.tw/exchangeReport/STOCK_DAY_ALL?response=open_data';
+  const res = UrlFetchApp.fetch(URL);
+  return Utilities.parseCsv(res.getContentText())
+    .filter((r) => list.includes(r[0]))
+    .map((r) => ({
       id: r[0],
       name: r[1],
       openPrice: parseFloat(r[4]),
       closePrice: parseFloat(r[7]),
     }));
-}
+};
+
+const DELAY_MSEC = 2000;
+const TRY_TIMES = 3;
 
 const fetchStocksClosedDays = () => {
-  const res = UrlFetchApp
-    .fetch('https://www.twse.com.tw/holidaySchedule/holidaySchedule?response=open_data');
-  return Utilities.parseCsv(res.getContentText())
+  const URL =
+    'https://www.twse.com.tw/holidaySchedule/holidaySchedule?response=open_data';
+  let content;
+  let tries = TRY_TIMES;
+  while (tries > 0) {
+    const res = UrlFetchApp.fetch(URL, { muteHttpExceptions: true });
+    const code = res.getResponseCode();
+    if (code >= 200 || code <= 299) {
+      content = res.getContentText();
+      break;
+    }
+    tries -= 1;
+    Utilities.sleep(DELAY_MSEC);
+  }
+  if (!content) return null;
+  return Utilities.parseCsv(content)
     .shift() // 移除欄位名稱的那一列
-    .map(r => ({
+    .map((r) => ({
       name: r[0],
-      //CSV檔案內的格式是民國年月日=>XXXYYZZ
+      // CSV檔案內的格式是民國年月日=>XXXYYZZ
       date: new Date(
-        1911 + parseInt(r[1].substring(0, 3)),
-        parseInt(r[1].substring(3, 5)) - 1, // 因為Date的month是從0開始,而不是1月的1開始
-        parseInt(r[1].substring(5)),
+        1911 + parseInt(r[1].substring(0, 3), 10),
+        parseInt(r[1].substring(3, 5), 10) - 1, // 因為Date的month是從0開始,而不是1月的1開始
+        parseInt(r[1].substring(5), 10)
       ),
       wday: TRA_CH_TO_TRIGGER_DAYOF_WEEKEY[r[2]] || r[2],
       comments: r[3],
     }));
-}
+};
 
-//https://developers.google.com/apps-script/guides/triggers/events
+// https://developers.google.com/apps-script/guides/triggers/events
 const onTriggered = (event) => {
   try {
-    //for test or trigger from editor
+    // for test or trigger from editor
     if (event === undefined) {
       log('施行觸發測試中...');
       notify('施行觸發測試中...');
-    }
-    else {
+    } else {
       const runAt = event2date(event);
       if (runAt.wday >= 6 || runAt.hour != 14) return;
 
       const closedDay = fetchStocksClosedDays();
       if (Array.isArray(closedDay)) {
-        const matched = closedDay.find(({ date }) =>
-          date.getDate() === runAt.date.getDate()
-          && date.getMonth() === runAt.date.getMonth()
-          && date.getFullYear() === runAt.date.getFullYear()
+        const matched = closedDay.find(
+          ({ date }) =>
+            date.getDate() === runAt.date.getDate() &&
+            date.getMonth() === runAt.date.getMonth() &&
+            date.getFullYear() === runAt.date.getFullYear()
         );
         if (matched) {
           log('今天是股票休假日', matched, runAt);
           return;
         }
-      }
-      else {
+      } else {
         log(`無法取得股票休息日資訊`);
         notify('無法取得股票休息日資訊');
-        //仍繼續執行
+        // 仍繼續執行
       }
     }
 
@@ -112,7 +115,11 @@ const onTriggered = (event) => {
 
     const filtered = fetchAndFilterStockByNumber(concerned);
     if (filtered.length !== concerned.length) {
-      log(`過濾後資料數量不吻合 f:${filtered.length} !== ${concerned.length} l:${concerned.join(',')}`);
+      log(
+        `過濾後資料數量不吻合 f:${filtered.length} !== ${
+          concerned.length
+        } l:${concerned.join(',')}`
+      );
     }
 
     if (filtered.length == 0) {
@@ -128,22 +135,24 @@ const onTriggered = (event) => {
       return '—';
     };
     const msg = filtered
-      .map(r => `${r.name}(${r.id}) 收盤:${r.closePrice} ${priceComp(r.closePrice, r.openPrice)}`)
+      .map(
+        (r) =>
+          `${r.name}(${r.id}) 收盤:${r.closePrice} ${priceComp(
+            r.closePrice,
+            r.openPrice
+          )}`
+      )
       .join('\n');
     log(msg);
     notify(msg);
-  }
-  catch (e) {
+  } catch (e) {
     log('發生錯誤', e);
     try {
       notify(e.message);
-    }
-    catch (e2) {
+    } catch (e2) {
       log('錯誤通知失敗', e2);
     }
   }
-}
-
-export {
-  onTriggered,
 };
+
+export { onTriggered };
